d69fc79fc365e10ebbea41f370940f55e6fabd09
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-07-25 02:29:02 +0000

    automatically backed up by learn

--- src/createStore.js
+++ src/createStore.js
@@ -18,6 +18,17 @@ export default function createStore(reducer) {
     };
 };
 
+function changeCount(state = { count: 0}, action) {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+    return { count: state.count + 1};
+
+    default:
+    return state;
+
+  }
+}
+
 function render() {
   const container = document.getElementById('container');
 }
