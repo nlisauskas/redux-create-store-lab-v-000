6c691246057b406218eb7fece3640e06a3434b7e
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-07-25 02:24:23 +0000

    automatically backed up by learn

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,10 @@
 export default function createStore(reducer) {
-  // add your code here
+    let state;
+
+    function dispatch(action) {
+      state = reducer(state, action);
+      if (store) render()
+    }
 }
 
 function render() {
